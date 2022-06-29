841f9a4e476d1e4a43ecca62b3ffbe1d89c33feb
badace <ali.belhacene@yahoo.fr>
2022-06-29 10:37:20 +0200

    refactoring: create molecules: label+input

---
+++ client/src/infrastructure/view/components/molecules/LabelInput/LabelInput.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Input } from '../../atoms/Input/Input';
+import { Label } from '../../atoms/Label/Label';
+
+export type PropsLabelInput = {
+  label: string;
+  type: React.HTMLInputTypeAttribute | undefined;
+  value: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  className?: string;
+  id?: string;
+  placeholder?: string;
+};
+
+export const LabelInput: React.FC<PropsLabelInput> = ({ ...props }) => {
+  return (
+    <div className="input-label">
+      <Label label={props.label} />
+      <Input {...props} />
+    </div>
+  );
+};
