11de37cb44625c308bf41b468d49ad0db8a1cbdf
rapatte <agayoso@wemanity.com>
2022-07-18 15:23:01 +0200

    refactor: logic in the listing component

--- client/src/infrastructure/view/pages/missionList/missions/index.tsx
+++ client/src/infrastructure/view/pages/missionList/missions/index.tsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { missionServices } from 'application';
-import { useMission } from 'infrastructure/view/hooks/UseMissions';
-import { Mission } from 'domain/mission/mission';
-import { missionList } from 'infrastructure/view/store/Mission/mission.actions';
+import React from 'react';
 import { SearchBar, ListingMissionCards } from 'infrastructure/view/components';
 
-export const Missions = ({ setDisplay, setProp }) => {
-  const { state, dispatch } = useMission();
-  const [catalog, setCatalog] = useState<Mission[]>([]);
-
-  useEffect(() => {
-    const missions: any = missionServices
-      .getMissions()
-      .then(data => dispatch(missionList(data)));
-    setCatalog(missions);
-  }, []);
-
-  useEffect(() => {
-    setCatalog(state.catalog);
-  }, [state.catalog]);
-
+export const Missions = () => {
   return (
     <>
-      <SearchBar />
-      <ListingMissionCards
-        setProp={setProp}
-        title="Les Missions"
-        cardType="mission"
-        props={catalog}
-        setDisplay={setDisplay}
-      />
+      <SearchBar placeholder="Chercher une mission" />
+      <ListingMissionCards title="Les Missions" cardType="mission" />
     </>
   );
 };
