8affcb7900148d8e27652875ef19b9739aff6ad5
badace <ali.belhacene@yahoo.fr>
2022-07-22 16:40:52 +0200

    refactoring: listingCards cooperator

--- client/src/infrastructure/view/pages/cooperator/mainCooperator/index.tsx
+++ client/src/infrastructure/view/pages/cooperator/mainCooperator/index.tsx
@@ -1,42 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   ListingCooperatorCards,
   SearchBar,
   Button,
 } from 'infrastructure/view/components';
-import { useCooperator } from 'infrastructure/view/hooks/UseCooperators';
-import { Cooperator } from '../../../../../domain/cooperator/cooperator';
-import cooperatorServices from 'application/cooperator/cooperator.factory';
-import { cooperatorList } from 'infrastructure/view/store/Cooperator/cooperator.actions';
 import { Link } from 'react-router-dom';
 
-export const Cooperators = ({ setDisplay, setProp }) => {
-  const { state, dispatch } = useCooperator();
-  const [catalog, setCatalog] = useState<Cooperator[]>([]);
-
-  useEffect(() => {
-    const cooperators: any = cooperatorServices
-      .getCooperators()
-      .then(data => dispatch(cooperatorList(data)));
-    setCatalog(cooperators);
-  }, []);
-
-  useEffect(() => {
-    setCatalog(state.catalog);
-  }, [state.catalog]);
-
+export const Cooperators = () => {
   return (
     <>
       <SearchBar placeholder="Chercher un coopérateur" />
       <Link to="/cooperateurs/ajouter">
         <Button className="addButton" label="Ajouter un coopérateur" />
       </Link>
-      <ListingCooperatorCards
-        cardType="cooperator"
-        props={catalog}
-        setDisplay={setDisplay}
-        setProp={setProp}
-      />
+      <ListingCooperatorCards title="Les coopérateurs" cardType="cooperator" />
     </>
   );
 };
