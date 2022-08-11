3d4a69382c9e0eef59d1f68857752926a9bd31d3
rapatte <agayoso@wemanity.com>
2022-08-11 17:18:07 +0200

    fix: removed authcontroller

--- api/src/modules/auth.module.ts
+++ api/src/modules/auth.module.ts
@@ -3,11 +3,9 @@ import { AuthService } from '../domain/auth/auth.service';
 import { UserModule } from './user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from '../exposition/auth/local.strategy';
-import { AuthController } from '../exposition/auth/auth.controller';
 
 @Module({
   imports: [UserModule, PassportModule],
-  controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
