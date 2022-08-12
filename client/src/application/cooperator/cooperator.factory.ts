f33e9bc64942b1d0f1458b63e73b6244de4945e7
rapatte <agayoso@wemanity.com>
2022-08-12 16:20:17 +0200

    fix: delete unused files

--- client/src/application/cooperator/cooperator.factory.ts
+++
@@ -1,7 +0,0 @@
-import { CooperatorService } from 'domain/cooperator/CooperatorService';
-import { cooperatorRepository } from 'infrastructure/cooperator/cooperator.repository';
-import { HttpAxios } from 'infrastructure/util/httpAxios';
-
-const repository = cooperatorRepository(HttpAxios);
-const cooperatorServices = CooperatorService(repository);
-export default cooperatorServices;
--- client/src/application/index.ts
+++ client/src/application/index.ts
@@ -1,5 +1,3 @@
-import cooperatorServices from './cooperator/cooperator.factory';
-import missionServices from './mission/mission.factory';
 import reviewServices from './review/review.factory';
 
-export { missionServices, cooperatorServices, reviewServices };
+export { reviewServices };
