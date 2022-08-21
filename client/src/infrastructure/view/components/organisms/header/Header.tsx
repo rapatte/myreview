c572eb374883a9efa0a26ef14c7d2302a3b6f190
rapatte <agayoso@wemanity.com>
2022-08-21 18:00:11 +0200

    feat: redirect on reviews when click on logo

--- client/src/infrastructure/view/components/organisms/header/Header.tsx
+++ client/src/infrastructure/view/components/organisms/header/Header.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { FunctionComponent } from 'react';
+import { useHistory } from 'react-router-dom';
 import { NavBar } from '../navbar/NavBar';
 
 interface HeaderProps {}
 
 const Header: FunctionComponent<HeaderProps> = () => {
+  const history = useHistory();
   return (
     <div className="header">
       <NavBar pageWrapId={'page-wrap'} outerContainerId={'App'} />
-      <img src="/MyReviewLogo.png" alt="logo" />
+      <img
+        onClick={() => history.push('/reviews')}
+        src="/MyReviewLogo.png"
+        alt="logo"
+      />
     </div>
   );
 };
